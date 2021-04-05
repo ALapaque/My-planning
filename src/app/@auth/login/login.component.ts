68891ff0601e6ebdb2db381883762cf3f85610db
Amaury <amaury.lapaque@gmail.com>
2021-04-05 16:36:18 +0200

    Product Backlog Item #30 - Mise en place page d'authentification
    - IN PROGRESS

--- src/app/@auth/login/login.component.ts
+++ src/app/@auth/login/login.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Location }          from '@angular/common';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
-})
+             selector: 'app-login',
+             templateUrl: './login.component.html',
+             styleUrls: [ './login.component.scss' ],
+           })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _location: Location,
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
+  public navigatePrevious(): void {
+    this._location.back();
+  }
 }
