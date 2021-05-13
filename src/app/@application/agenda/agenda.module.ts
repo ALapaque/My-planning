9c9b9bdb77956d7083aba097db5e00e0e8abf6f3
Amaury Lapaque <amaury.lapaque@gmail.com>
2021-05-13 16:11:22 +0200

    adding locale in progress

--- src/app/@application/agenda/agenda.module.ts
+++ src/app/@application/agenda/agenda.module.ts
@@ -13,6 +13,19 @@ import { AgendaTimelineComponent } from './agenda-timeline/agenda-timeline.compo
 import { AgendaComponent }         from './agenda.component';
 import { AgendaRouting }           from './agenda.routing';
 import { AgendaStandardComponent } from './agenda-standard/agenda-standard.component';
+import { L10n, loadCldr, setCulture }          from '@syncfusion/ej2-base';
+// @ts-ignore
+import * as numberingSystems       from 'cldr-data/supplemental/numberingSystems.json';
+// @ts-ignore
+import * as gregorian              from 'cldr-data/main/fr-BE/ca-gregorian.json';
+// @ts-ignore
+import * as numbers                from 'cldr-data/main/fr-BE/numbers.json';
+// @ts-ignore
+import * as timeZoneNames          from 'cldr-data/main/fr-BE/timeZoneNames.json';
+// @ts-ignore
+import * as EJ2_LOCALE             from '@syncfusion/ej2-locale/src/fr.json';
+
+loadCldr(numberingSystems, gregorian, numbers, timeZoneNames);
 
 @NgModule({
             declarations: [
