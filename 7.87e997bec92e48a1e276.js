8c32bfeb2c17c3e8f99d6aa7de2400635a813c8e
Amaury Lapaque <amaury.lapaque@gmail.com>
2021-05-23 09:49:38 +0200

    Auto-generated commit

--- 7.87e997bec92e48a1e276.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{xdUr:function(n,t,o){"use strict";o.r(t),o.d(t,"SettingsModule",function(){return c});var e=o("ofXK"),r=o("fXoL");let c=(()=>{class n{}return n.\u0275fac=function(t){return new(t||n)},n.\u0275mod=r.Nb({type:n}),n.\u0275inj=r.Mb({imports:[[e.c]]}),n})()}}]);
