9e18df0c708467b6fc85313a31befedf3f1136b9
Amaury Lapaque <amaury.lapaque@gmail.com>
2021-05-23 10:00:58 +0200

    added reload when there's a update

--- src/app/@homepage/section/features-content/features-content.component.ts
+++ src/app/@homepage/section/features-content/features-content.component.ts
@@ -2,82 +2,59 @@ import { Component, OnInit } from '@angular/core';
 import { SectionCard }       from '../../@shared/models/section-card.model';
 
 @Component({
-  selector: 'app-features-content',
-  templateUrl: './features-content.component.html',
-  styleUrls: ['./features-content.component.scss']
-})
+             selector: 'app-features-content',
+             templateUrl: './features-content.component.html',
+             styleUrls: [ './features-content.component.scss' ],
+           })
 export class FeaturesContentComponent implements OnInit {
 
   public cards: Array<SectionCard> = new Array<SectionCard>(
     new SectionCard(
       {
-        img: './assets/homepage/sections/cards/section-card-calendar.jpg',
         title: 'L\'agenda',
-        description: '<ul>' +
-                     '<li>1</li>' +
-                     '<li>2</li>' +
-                     '<li>3</li>' +
-                     '</ul>'
-      }
+        description: `<p>Retrouvez un agenda complet avec la possibilité d\'afficher vos événements dans différentes vues soit d\'un type timeline ou bien standard</p>`,
+      },
     ),
     new SectionCard(
       {
-        img: './assets/homepage/sections/cards/section-card-calendar.jpg',
-        title: 'L\'agenda',
-        description: '<ul>' +
-                     '<li>1</li>' +
-                     '<li>2</li>' +
-                     '<li>3</li>' +
-                     '</ul>'
-      }
+        title: 'Réunions',
+        description: `Suivez vos réunions en direct grace à l'encodage de vos rapport directement sur vos événements`,
+      },
     ),
     new SectionCard(
       {
-        img: './assets/homepage/sections/cards/section-card-calendar.jpg',
         title: 'L\'agenda',
         description: '<ul>' +
                      '<li>1</li>' +
                      '<li>2</li>' +
                      '<li>3</li>' +
-                     '</ul>'
-      }
+                     '</ul>',
+      },
     ),
     new SectionCard(
       {
-        img: './assets/homepage/sections/cards/section-card-calendar.jpg',
         title: 'L\'agenda',
         description: '<ul>' +
                      '<li>1</li>' +
                      '<li>2</li>' +
                      '<li>3</li>' +
-                     '</ul>'
-      }
+                     '</ul>',
+      },
     ),
     new SectionCard(
       {
-        img: './assets/homepage/sections/cards/section-card-calendar.jpg',
         title: 'L\'agenda',
         description: '<ul>' +
                      '<li>1</li>' +
                      '<li>2</li>' +
                      '<li>3</li>' +
-                     '</ul>'
-      }
-    ),
-    new SectionCard(
-      {
-        img: './assets/homepage/sections/cards/section-card-calendar.jpg',
-        title: 'L\'agenda',
-        description: '<ul>' +
-                     '<li>1</li>' +
-                     '<li>2</li>' +
-                     '<li>3</li>' +
-                     '</ul>'
-      }
+                     '</ul>',
+      },
     ),
   );
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
