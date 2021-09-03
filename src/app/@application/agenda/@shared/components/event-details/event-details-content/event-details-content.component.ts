1438be16e58f8c497f361e7c9ca8a86bf5216925
Amaury <amaury.lapaque@gmail.com>
2021-09-03 20:45:40 +0200

    added comments

--- src/app/@application/agenda/@shared/components/event-details/event-details-content/event-details-content.component.ts
+++ src/app/@application/agenda/@shared/components/event-details/event-details-content/event-details-content.component.ts
@@ -1,5 +1,9 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {SchedulerEvent} from '../../../models/scheduler-event.model';
+import {Comment} from '../../../../../../@shared/models/comment.model';
+import {AuthService} from '../../../../../../@shared/services/auth.service';
+import {BreakpointObserver} from '@angular/cdk/layout';
+import {NbStepperComponent} from '@nebular/theme';
 
 @Component({
   selector: 'app-event-details-content',
@@ -7,12 +11,19 @@ import {SchedulerEvent} from '../../../models/scheduler-event.model';
   styleUrls: ['./event-details-content.component.scss']
 })
 export class EventDetailsContentComponent implements OnInit {
+  @ViewChild('stepper') public nbStepper: NbStepperComponent;
   @Input() public event!: SchedulerEvent;
 
-  constructor() {
+  constructor(
+    public breakpointObserver: BreakpointObserver,
+    private _authService: AuthService
+  ) {
   }
 
   ngOnInit(): void {
   }
 
+  isSameUser(msg: Comment): boolean {
+    return this._authService.user.id === msg.user.id;
+  }
 }
